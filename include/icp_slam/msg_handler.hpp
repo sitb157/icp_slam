af43a7e4e0c8cd131b0be9cc12bf3251030e3448
sitb157 <sitb157@gmail.com>
2023-10-23 17:10:21 +0900

    update add converToXYZ

--- include/icp_slam/msg_handler.hpp
+++ include/icp_slam/msg_handler.hpp
@@ -3,6 +3,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/point_cloud2_iterator.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 #include <memory>
 #include <deque>
@@ -21,11 +22,15 @@ class MsgHandler {
         void insertPointCloudMsg(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 
         // Get pointcloud queue size 
-        std::size_t getPointCloudQueueSize();
+        size_t getPointCloudQueueSize();
 
     private:
 
         std::shared_ptr<std::deque<sensor_msgs::msg::PointCloud2::SharedPtr>> pointcloudQueue_;
+        std::shared_ptr<std::vector<std::vector<int>>> xyzVectors_;
+
+        // Convert Point Cloud Msg to XYZ
+        std::vector<std::vector<float>> convertToXYZ(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 };
 
 
--- src/msg_handler.cpp
+++ src/msg_handler.cpp
@@ -9,6 +9,20 @@ void MsgHandler::insertPointCloudMsg(const sensor_msgs::msg::PointCloud2::Shared
     pointcloudQueue_->push_back(msg);
 }
 
-std::size_t MsgHandler::getPointCloudQueueSize() {
+size_t MsgHandler::getPointCloudQueueSize() {
     return pointcloudQueue_->size();
+}
+
+std::vector<std::vector<float>> MsgHandler::convertToXYZ(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
+
+    const size_t num_points = msg->height * msg->width;
+    std::vector<std::vector<float>> xyz_output;
+    sensor_msgs::PointCloud2Iterator<float> iter_x(*msg, "x");
+    sensor_msgs::PointCloud2Iterator<float> iter_y(*msg, "y");
+    sensor_msgs::PointCloud2Iterator<float> iter_z(*msg, "z");
+    for (size_t i = 0; i < num_points; ++i, ++iter_x, ++iter_y, ++iter_z) {
+        xyz_output.push_back(std::vector<float>{*iter_x, *iter_y, *iter_z});
+        
+    } 
+    return xyz_output;
 }
